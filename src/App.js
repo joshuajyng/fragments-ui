f5f090127c5071e1d5fdea493946f6b7451bcbbf
Joshua Ng <joshuajyng@gmail.com>
2022-09-18 17:19:05 -0400

    setup login and logout

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {Component} from 'react'
+import {Auth, getUser} from './auth'
+
+class App extends Component {
+  state = {
+    username: null
+  };
+  login() {
+    Auth.federatedSignIn();
+  }
+  logout(){
+    Auth.signOut();
+  }
+  componentDidMount(){
+    this.init()
+  }
+  init = async() => {
+    const user = await getUser()
+    console.log(user)
+    this.setState({username: user.username})
+  }
+  render (){
+    return (
+      <div className="App">
+        <h1>Fragments-UI</h1>
+        <p hidden={this.state.username === null?true:false}>Hi {this.state.username}</p>
+        <button onClick={this.login}>Log in</button>
+        <button onClick={this.logout} disabled={this.state.username === null?true:false}>Log out</button>
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
